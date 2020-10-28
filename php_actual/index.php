c1bb16c8433b1107931c526c592c3ca0b76b1a0c
Eravate <arekarino@gmail.com>
2020-10-28 12:40:24 +0100

    bruh

--- php_actual/index.php
+++ php_actual/index.php
@@ -26,9 +26,43 @@
         }
       }
       a {
+        clear: both;
         color: black;
         text-decoration: none; /* no underline */
       }
+      .xright {
+        float: right;
+      }
+      .x {
+        position: relative;
+        width: 27px;
+        height: 27px;
+        border: 2px solid #eef5df;
+        background-color: #ff5248;
+        border-radius: 50%;
+      }
+      .x::before, .x::after {
+        position: absolute;
+        top: 10px;
+        left: 5px;
+        width: 13px;
+        height: 3px;
+        content: "";
+        background-color: #eef5df;
+        display: none;
+        }
+      .x::before {
+        -ms-transform: rotate(-45deg);
+        -webkit-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+        }
+      .x::after {
+        -ms-transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+        transform: rotate(45deg);
+        }
+      .x:hover { cursor: pointer; }
+      .x:hover::before, .x:hover::after { display: block; }
     </style>
     <!-- Custom styles for this template -->
     <link href="css/jumbotron.css" rel="stylesheet">
@@ -63,60 +97,99 @@
   <div class="jumbotron">
     <div class="container">
     <?php 
+    #Cada variable corresponde a una diferente combinación de entradas de información GET y POST
     $fichero=$_GET['fichero'];
     $buscar=$_GET['buscar'];
     $enviarCat=$_GET['enviarCat'];
+    $eliminarCat = $_GET['eliminarCat'];
     $enviarArch=$_POST['enviarArch'];
-    if (!isset($fichero)) {
-      if (isset($enviarCat)) {
-        $nombreCat = $_GET['nombreCat'];
-        $old_umask = umask(0);
-        mkdir("archivos/$nombreCat",0777);
-        umask($old_umask);
-        header("Refresh:0; url=index.php");
-      }
-      if (isset($enviarArch)) {
-        $nombreArch = $_POST['nombreArch'];
-        $categoriaArch = $_POST['categoriaArch'];
-        $dir  = "archivos/$categoriaArch/";
-        $dirfich = $dir . basename($_FILES["arch"]["name"]);
-        $imageFileType = strtolower(pathinfo($dirfich,PATHINFO_EXTENSION));
-        $uploadOk = 1;
-        if($imageFileType != "exe" ) {
-                        $error = "Error, compruebe que el archivo sea un EXE";
-                        $uploadOk = 0;
-                    }
-        if ($uploadOk==1 && move_uploaded_file($_FILES["arch"]["tmp_name"], $dir.$nombreArch.".exe")) {
-          $error = "Archivo Subido";
-        } 
-      }
-      if (isset($buscar)) {
-        $contenido=$_GET['contenido'];
-          echo ("<h1>Resultado de la querry '$contenido': <br><br>");
-          echo ("<ul>");
-          busqExes($contenido);
-          echo ("</ul>");
-      } else {
-          echo ("<h1 class='display-3'>Bienvenido!</h1><p>Aquí puede descargar todos los programas que considere necesarios, desde el servidor local!</p>");
-          echo ("<br><h2>Crear nueva categoría</h2>");
-          echo ("<p>Desea crear una nueva categoría? rellena el siguiente formulario:");
-          echo ("<form>Nombre Categoría: <input type='text' name='nombreCat' id='nombreCat'> <input type='submit' name='enviarCat' value='Enviar'></form>");
-          echo ("<br><br><h2>Subir un Archivo</h2>");
-          echo ("<p>Quieres que tengamos un archivo en local? rellena el siguiente formulario:");
-          echo ("<form  enctype='multipart/form-data' method='POST'>Nombre Archivo: <input type='text' name='nombreArch' id='nombreArch'><br><br>Categoria: <select name='categoriaArch'>");
-          cargarDirsSelect();
-          echo ("</select><br><br>Archivo: <input type='file' id='arch' name='arch'><br><br><input type='submit' name='enviarArch' value='Enviar'> ");
-          if (isset($error)) {
-            echo($error);
-          }
-          echo ("</form>");
-      }
-    } else {
-        echo ("<h1>Archivos a descargar en ".$fichero.": </h1><br><br>");
-        echo ("<ul>");
-          cargarExes($fichero);
-        echo ("</ul>");
+    $eliminarArch = $_GET['eliminarArch'];
+    #Si la información viene mediante el GET de añadir Categoria
+    if (isset($enviarCat)) {
+      $nombreCat = $_GET['nombreCat'];
+      $old_umask = umask(0);
+      mkdir("archivos/$nombreCat",0777);
+      umask($old_umask);
+      header("Refresh:0; url=index.php");
     }
+    #Si la información viene mediante el GET de eliminar Categoria
+    if (isset($eliminarCat)) {
+      $nombreCat = $_GET['nombreCat'];
+      if (rmdir("archivos/$nombreCat")) {
+        header("Refresh:0; url=index.php");
+      } else {
+        echo '<script>';
+        echo 'alert("Error, esta categoría contiene archivos")';
+        echo '</script>';
+      }
+    }
+    #Si la información viene mediante el GET de eliminar Archivo
+    if (isset($eliminarArch)) {
+      $nombreArch = $_GET['nombreArch'];
+      echo $nombreArch;
+      unlink($nombreArch);
+      header("Refresh:0; url=index.php");
+    }
+    #Si la información viene mediante el GET de añadir Archivo
+    if (isset($enviarArch)) {
+      $nombreArch = $_POST['nombreArch'];
+      if ($nombreArch == "") {
+          $error = "Error, no ha introducido nombre de archivo";
+      } else {
+        $categoriaArch = $_POST['categoriaArch'];
+        if ($categoriaArch == "") {
+          $error ="Error, eliga una categoría antes de subir el archivo";
+        } else {
+          $dir  = "archivos/$categoriaArch/";
+          $dirfich = $dir . basename($_FILES["arch"]["name"]);
+          $imageFileType = strtolower(pathinfo($dirfich,PATHINFO_EXTENSION));
+          $uploadOk = 1;
+          if($imageFileType != "exe" && $imageFileType != "gz" && $imageFileType != "rar" && $imageFileType != "zip") {
+            $error = "Error, compruebe que el archivo sea un EXE, TAR.GZ, RAR o ZIP";
+            $uploadOk = 0;
+          }
+          if ($imageFileType == "gz") {
+            $nombreArch .= ".tar";
+          }
+          $old_umask = umask(0);
+          if ($uploadOk==1 && move_uploaded_file($_FILES["arch"]["tmp_name"], $dir.$nombreArch.".".$imageFileType)) {
+            $error = "Archivo Subido";
+          } 
+          umask($old_umask);
+        }
+      }
+    }
+    #Resultado de busqueda
+    if (isset($buscar)) {
+      $contenido=$_GET['contenido'];
+        echo ("<h1>Resultado de la querry '$contenido': </h1><br><br>");
+        echo ("<ul>");
+        busqExes($contenido);
+        echo ("</ul>");
+    } else if (isset($fichero)) {
+      #Si la información es de una categoria | el contenido de una categoría
+      echo ("<h1>Archivos a descargar en ".$fichero.": </h1><a href='index.php?eliminarCat=true&nombreCat=$fichero'><div class='x'></div></a><br><br>");
+      echo ("<ul>");
+        cargarExes($fichero);
+      echo ("</ul>");
+    } else {
+      #Si la información tiene que ser la por defecto
+      echo ("<h1 class='display-3'>Bienvenido!</h1><p>Aquí puede descargar todos los programas que considere necesarios, desde el servidor local!</p><p>Las carpetas de las categorías se crean en: archivos.</p>");
+      echo ("<br><h2>Crear nueva categoría</h2>");
+      echo ("<p>Desea crear una nueva categoría? rellena el siguiente formulario:");
+      echo ("<form method='GET'>Nombre Categoría: <input type='text' name='nombreCat' id='nombreCat'> <input type='submit' name='enviarCat' value='Enviar'></form>");
+      echo ("<br><br><h2>Subir un Archivo</h2>");
+      echo ("<p>Quieres que tengamos un archivo en local? rellena el siguiente formulario:");
+      echo ("<form  enctype='multipart/form-data' method='POST'>Nombre Archivo: <input type='text' name='nombreArch' id='nombreArch'><br><br>Categoria: <select name='categoriaArch'>");
+      cargarDirsSelect();
+      echo ("</select><br><br>Archivo: <input type='file' id='arch' name='arch'><br><br><input type='submit' name='enviarArch' value='Enviar'> ");
+      if (isset($error)) {
+        echo($error);
+      }
+      echo ("</form>");
+    }
+    
+    
     ?>
       
     </div>
--- php_actual/php/carArchivos.php
+++ php_actual/php/carArchivos.php
@@ -1,13 +1,22 @@
 <?php
+    #función para cargar las categorías
     function cargarDirs() {
-        $d = dir("archivos");
-        while (false !==  ($fichero=$d->read())) {
-            if(is_dir("archivos/".$fichero) && $fichero!="." && $fichero!="..") {
-                echo ("<li class='nav-item'><a class='nav-link' href='?fichero=".$fichero."'>".$fichero."</a></li>");
+        if (file_exists("archivos")) {
+            $d = dir("archivos");
+            while (false !==  ($fichero=$d->read())) {
+                if(is_dir("archivos/".$fichero) && $fichero!="." && $fichero!="..") {
+                    echo ("<li class='nav-item'><a class='nav-link' href='?fichero=".$fichero."'>".$fichero."</a></li>");
+                }
             }
+        } else {
+            $old_umask = umask(0);
+            mkdir("archivos",0777);
+            umask($old_umask);
         }
+        
     }
 
+    #función para cargar las categorías del select
     function cargarDirsSelect() {
         $d = dir("archivos");
         while (false !==  ($fichero=$d->read())) {
@@ -17,18 +26,20 @@
         }
     }
     
+    #función para cargar los archivos de una categoría
     function cargarExes($directorio) {
         $d = dir("archivos/$directorio");
         while (false !==  ($fichero=$d->read())) {
             if(!is_dir("archivos/$directorio".$fichero) && $fichero!="." && $fichero!="..") {
                 list($nom,$ext) = explode (".",strval($fichero));
                 $nom = strtoupper($nom);
-                echo ("<li style='font-size:20px;'><a href='archivos/$directorio/$fichero'>$nom</a></li>");
+                echo ("<li style='font-size:20px;'><a href='archivos/$directorio/$fichero'>$nom</a><a href='index.php?eliminarArch=true&nombreArch=archivos/$directorio/$fichero'><div class='x xright'></div></a></li>");
             }
             
         }
     }
 
+    #función para obtener resultados de un querry
     function busqExes($nombre) {
         $nombre = strtoupper($nombre);
         $d = dir("archivos");
@@ -40,7 +51,7 @@
                         list($nom,$ext) = explode (".",strval($ficheroInterno));
                         $nom = strtoupper($nom);
                         if (strpos($nom,$nombre) !== false) {
-                            echo ("<li style='font-size:20px;'><a href='archivos/$fichero/$ficheroInterno'>$nom</a></li>");
+                            echo ("<li style='font-size:20px;'><a href='archivos/$fichero/$ficheroInterno'>$nom</a><a href='index.php?eliminarArch=true&nombreArch=archivos/$fichero/$ficheroInterno'><div class='x xright'></div></a></li>");
                         }
                     }
                 }
